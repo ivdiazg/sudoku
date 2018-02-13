5336c0724cf58aeecf3d5a7caf3e9e6cf288f0c3
Paula Ponce <paula.ponce.zuniga@gmail.com>
2018-02-13 12:12:31 -0300

    estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CuadroSudokuComponent } from './cuadro-sudoku/cuadro-sudoku.component';
 import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
@@ -17,7 +18,8 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
